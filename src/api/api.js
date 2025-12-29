f3f185b18033d5197cd24b1d21edaba8a21e74d9
favasgh <mfavax@gmail.com>
2025-12-29 15:23:54 +0530

    fix api base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // adjust if your backend uses different URL
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 15000,
 });
 
