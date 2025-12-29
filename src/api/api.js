65e2fb52542bdc826b732f4d1373362f842398d7
favasgh <mfavax@gmail.com>
2025-12-29 17:07:19 +0530

    Fix production API baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://printopayrollbackend.onrender.com/api",
   timeout: 15000,
 });
 
